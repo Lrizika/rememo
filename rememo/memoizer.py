77524873c4f94cee39e5f336ed753848a63fae0e
larkeith <llarkeith@gmail.com>
2023-01-14 21:11:48 -0800

    Convert memoizer to use pickle for default serialization method

--- rememo/memoizer.py
+++ rememo/memoizer.py
@@ -1,5 +1,6 @@
 
 import json
+import pickle
 from typing import Iterable, Any, Hashable, Tuple, Optional
 from functools import wraps
 
@@ -29,69 +30,36 @@ class Memoizer:
 
 	def __init__(
 			self,
-			use_generalized_make_hashable: bool = False,
+			make_hashable_func: callable = pickle.dumps,
 			sort_kwargs: bool = True,
 			kwarg_sort_func: callable = sorted,
-			accept_dict_args: bool = True,
-			sort_dict_args: bool = True,
-			dict_args_sort_func: callable = sorted,
-			recurse_dict_args: bool = True,
-			accept_dict_kwargs: bool = True,
 			**kwargs
 	):
 		self.results_cache = {}
 
-		self.use_generalized_make_hashable = use_generalized_make_hashable
+		self.make_hashable_func = make_hashable_func
 		self.sort_kwargs = sort_kwargs
 		self.kwarg_sort_func = kwarg_sort_func
-		self.accept_dict_args = accept_dict_args
-		self.sort_dict_args = sort_dict_args
-		self.dict_args_sort_func = dict_args_sort_func
-		self.recurse_dict_args = recurse_dict_args
-		self.accept_dict_kwargs = accept_dict_kwargs
 
 	def process_params(
 			self, param_args: list, param_kwargs: dict
-	) -> Tuple[Tuple[Any, ...], Tuple[Tuple[Any, Any], ...]]:
+	) -> Hashable:
 		'''
 		Converts function parameters to a hashable form.
+		By default, this uses pickle.dumps
 
 		See Memoizer.__init__ for processing options.
 
 		Args:
-			param_args (list): args to convert to a hashable form:
-				Converted form is a tuple of arguments
-			param_kwargs (dict): kwargs to convert to a hashable form:
-				Converted form is a tuple of tuples of key-value pairs
+			param_args (list): args to convert to a hashable form
+			param_kwargs (dict): kwargs to convert to a hashable form
 
 		Returns:
-			Tuple[Tuple[Any, ...], Tuple[Tuple[Any, Any], ...]]: A hashable
-				tuple of args and kwargs.
+			Hashable: A hashable form of the input parameters.
+				For the default method of pickle.dumps, this is a bytes object
 		'''
 
-		if self.use_generalized_make_hashable:
-			return Memoizer.make_hashable((param_args, param_kwargs))
-
-		if self.accept_dict_args:
-			processed_args = tuple([
-				Memoizer._dict_to_hashable_if_dict(
-					arg,
-					sort_after=self.sort_dict_args,
-					sort_func=self.dict_args_sort_func,
-					recurse=self.recurse_dict_args,
-				) for arg in param_args
-			])
-		else:
-			processed_args = tuple(param_args)
-
-		processed_kwargs = Memoizer.dict_to_hashable(
-			param_kwargs,
-			sort_after=self.sort_kwargs,
-			sort_func=self.kwarg_sort_func,
-			recurse=self.accept_dict_kwargs,
-		)
-
-		return (processed_args, processed_kwargs)
+		return self.make_hashable_func((param_args, param_kwargs))
 
 	def _call_and_add_result(self, function: callable, *args, **kwargs) -> None:
 		'''
@@ -191,90 +159,4 @@ class Memoizer:
 	memo = memoized
 	cache = memoized
 
-	@staticmethod
-	def dict_to_hashable(
-			dict_to_hash: dict,
-			recurse: bool = False,
-			sort_after: bool = False,
-			sort_func: callable = sorted
-	) -> Tuple[Tuple[Hashable, Hashable]]:
-		'''
-		Converts a dict to a tuple of tuples, such that it may be hashed.
-
-		Args:
-			dict_to_hash (dict): The dict that should be made hashable.
-			recurse (bool, optional): Defaults to False. Whether to recurse
-				down sub-dictionaries.
-			sort_after (bool, optional): Defaults to False. Whether to sort
-				the dict afterwards. If true, uses `sort_func`.
-			sort_func (callable, optional): Defaults to `sorted`. Function to
-				call on the dict to sort it.
-
-		Returns:
-			Tuple[Tuple[Hashable, Hashable]]
-		'''
-
-		if recurse:
-			result = (
-				(
-					Memoizer._dict_to_hashable_if_dict(
-						key, recurse=True, sort_after=sort_after, sort_func=sort_func
-					),
-					Memoizer._dict_to_hashable_if_dict(
-						value, recurse=True, sort_after=sort_after, sort_func=sort_func
-					)
-				) for key, value in dict_to_hash.items()
-			)
-		else:
-			result = ((key, value) for key, value in dict_to_hash.items())
-
-		if sort_after:
-			return tuple(sort_func(result))
-		else:
-			return result
-
-	@staticmethod
-	def _dict_to_hashable_if_dict(potential_dict: Any, **kwargs) -> Any:
-		'''
-		Helper function for `dict_to_hashable`. Calls `dict_to_hashable` on
-			the `potential_dict` if it's a dict, returns it unchanged otherwise.
-
-		Args:
-			potential_dict (Any): Object that may or may not be a dict.
-
-		Returns:
-			Any: Either `potential_dict` or the results of `dict_to_hashable`.
-		'''
-
-		if isinstance(potential_dict, dict):
-			return Memoizer.dict_to_hashable(potential_dict, **kwargs)
-		else:
-			return potential_dict
-
-	@staticmethod
-	def make_hashable(obj: Any) -> Hashable:
-		'''
-		A generalized function for making objects hashable.
-			Not the default method due to potential unexpected behaviour, but
-			provides a more powerful alternative than dict_to_hashable.
-
-		WARNING: Fails for self-referential objects, may occasionally cause
-			falsely-equating results.
-
-		Args:
-			obj (Any): The object to make hashable
-
-		Returns:
-			Hashable: The object, hopefully made hashable
-		'''
-
-		try:
-			hash(obj)  # Isinstance Hashable fails on nested objects
-			return obj
-		except TypeError:
-			if isinstance(obj, dict):
-				return tuple(sorted((Memoizer.make_hashable((key, value)) for key, value in obj.items())))
-			elif isinstance(obj, Iterable):
-				return tuple((Memoizer.make_hashable(value) for value in obj))
-			return json.dumps(obj)
 
