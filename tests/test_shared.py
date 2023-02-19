e327c4634327dd69dc6ebee916f3019a14364e40
larkeith <llarkeith@gmail.com>
2023-02-18 17:36:42 -0800

    Add RemoteCache tests

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,8 +1,10 @@
 
 import unittest
 import logging
+import multiprocessing.managers
 
 import rememo
+from rememo.templates.shared import RemoteCache
 
 from tests import test_Memoizer
 
@@ -10,7 +12,43 @@ logger = logging.getLogger(__name__)
 
 
 class TestRemoteCache(unittest.TestCase):
-	pass
+	def test_create(self):
+		cache = RemoteCache()
+		cache2 = RemoteCache()
+		self.assertTrue(cache.is_host)
+		self.assertFalse(cache2.is_host)
+
+	def test_host_failover(self):
+		cache = RemoteCache()
+		cache2 = RemoteCache()
+		self.assertTrue(cache.is_host)
+		self.assertFalse(cache2.is_host)
+		del cache
+		cache2.ping()
+		self.assertTrue(cache2.is_host)
+
+	def test_set_get(self):
+		cache = RemoteCache()
+		cache['key'] = 3
+		self.assertEqual(cache['key'], 3)
+		with self.assertRaises(multiprocessing.managers.RemoteError):
+			cache['unset_key']
+
+	def test_get_shared(self):
+		cache = RemoteCache()
+		cache['key'] = 'val'
+		cache2 = RemoteCache()
+		self.assertEqual(cache2['key'], 'val')
+
+	def test_preprocess_key(self):
+		def preprocessor(key):
+			return key[::-1]
+		cache = RemoteCache(key_preprocessor=preprocessor)
+		cache['key'] = 'val'
+		self.assertEqual(cache['key'], 'val')
+		self.assertEqual(cache.manager['yek']._getvalue(), 'val')
+		cache2 = RemoteCache()
+		self.assertEqual(cache2['yek'], 'val')
 
 
 class TestSharedMemoizer(test_Memoizer.TestMemoizer):
